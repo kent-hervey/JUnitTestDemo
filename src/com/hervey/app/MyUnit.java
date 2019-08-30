166e76b1d88741ce4ec0a52981d8bb4e63d18cf7
Kent Hervey <kent.hervey@gmail.com>
2019-08-30 16:10:14 -0500

    removed main

--- src/com/hervey/app/MyUnit.java
+++ src/com/hervey/app/MyUnit.java
@@ -2,39 +2,30 @@ package com.hervey.app;
 
 public class MyUnit {
 
-    private Object MyUnit;
+	private Object MyUnit;
 
-	public String concatenate(String one, String two){
-        return one + two;
-    }
-    
-    public String[] getTheStringArray() {
-    	String[] expectedArray = {"one", "two", "three"};
-    	
-    	return expectedArray;
-    }
-    
-    
-    public Boolean getTheBoolean() {
-    	return false;
-    }
-    
-    public Object getTheObject() {
-    	Object nothing = MyUnit;
-    	return nothing;
-    }
-    
-    
-    
-    public Object getTheSameObject() {
-    	Object nothing = MyUnit;
-    	return nothing;
-    }
-    
-    
-    
-    
-    
-    
-    
+	public String concatenate(String one, String two) {
+		return one + two;
+	}
+
+	public String[] getTheStringArray() {
+		String[] expectedArray = { "one", "two", "three" };
+
+		return expectedArray;
+	}
+
+	public Boolean getTheBoolean() {
+		return false;
+	}
+
+	public Object getTheObject() {
+		Object nothing = MyUnit;
+		return nothing;
+	}
+
+	public Object getTheSameObject() {
+		Object nothing = MyUnit;
+		return nothing;
+	}
+
 }
--- test/java/com/hervey/app/MyMatcherTest.java
+++ test/java/com/hervey/app/MyMatcherTest.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 
 public class MyMatcherTest {
 
-    @Test
-    public void testWithMatchers() {
-        assertThat("this string", is("this string"));
-        assertThat(123, is(123));
-    }
+	@Test
+	public void testWithMatchers() {
+		assertThat("this string", is("this string"));
+		assertThat(123, is(123));
+	}
 }
